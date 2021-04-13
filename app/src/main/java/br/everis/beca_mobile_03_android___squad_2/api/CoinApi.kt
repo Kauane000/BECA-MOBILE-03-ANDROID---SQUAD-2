011b8cfef4f6d650ed763f039935f2989a4f2f05
kauane silva <kauane.nep@gmail.com>
2021-04-13 15:47:51 -0300

    Primeira chamada Api na interface

--- app/src/main/java/br/everis/beca_mobile_03_android___squad_2/api/CoinApi.kt
+++ app/src/main/java/br/everis/beca_mobile_03_android___squad_2/api/CoinApi.kt
@@ -1,4 +1,12 @@
 package br.everis.beca_mobile_03_android___squad_2.api
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface CoinApi {
+
+    @GET("assets?apiKey=CFB9107C-F454-4F93-B412-C7F15E3D284D")
+    fun getAllListCoin(): Call<>
+
+
 }
