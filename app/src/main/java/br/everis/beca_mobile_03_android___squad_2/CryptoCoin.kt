c6f41ffafe8925409ed238de0b10fc912c844f5a
kauane silva <kauane.nep@gmail.com>
2021-04-13 16:01:19 -0300

    Criação do constutor para os atributos vindos da api

---
+++ app/src/main/java/br/everis/beca_mobile_03_android___squad_2/CryptoCoin.kt
@@ -0,0 +1,11 @@
+package br.everis.beca_mobile_03_android___squad_2
+
+import com.google.gson.annotations.SerializedName
+
+data class CryptoCoin (
+
+    @SerializedName("asset_id") var asset_id: String?,
+    @SerializedName("name") var name: String?,
+
+
+)
